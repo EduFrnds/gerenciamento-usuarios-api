c155779ddf3e45a50d462adbab9b9b488caf6df0
Eduardo <edufernandes.contato@gmail.com>
2025-05-15 14:02:42 -0300

    ACRH_002 - Reestruturação da arquitetura Hexagonal, inclusao da camada domain

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from app.logging_config import configure_logging
 
-# Configurar o logger e obtê-lo
-logger = configure_logging()  # Agora 'logger' é o objeto de logging
-
+logger = configure_logging()
 app = FastAPI()
 
-@app.get("/")
+@app.get("/testeapi")
 def read_root():
     logger.info("Projeto FASTAPI iniciado.")
     return {
-        "message": "API de Gerenciamento de Usuários"
+        "mensagem": "API de Gerenciamento de Usuários"
         }
